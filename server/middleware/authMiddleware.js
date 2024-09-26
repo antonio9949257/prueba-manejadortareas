04c9f9971f17d9b3d8608516823f132064338174
armin <antonio9949257@gmail.com>
2024-09-26 16:39:48 -0400

    forma correcta de extrer el token en el middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 function authMiddleware(req, res, next) {
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Extrae el token del header
+    const token = authHeader && authHeader.split(' ')[1]; 
 
     if (!token) {
         return res.status(401).json({ message: 'No se proporciona el token' });
@@ -15,8 +15,8 @@ function authMiddleware(req, res, next) {
             return res.status(403).json({ message: 'Token no válido' });
         }
         
-        req.user = decoded; // Guarda los datos del usuario decodificados
-        next(); // Llama al siguiente middleware
+        req.user = decoded; 
+        next();
     });
 }
 
